baa8b2e4e3f6d71a270ccc71aab07b3bd2e84ad8
Alijewa25 <tahminaalijewa@gmail.com>
2026-07-15 15:08:13 +0400

    feat: implement get_page method in Server class for simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-"""
-Məlumat bazasını idarə edən və sadə səhifələmə tətbiq edən modul.
-"""
 import csv
 from typing import List
 
-# Əvvəlki tapşırıqdakı funksiyanı import edirik
 index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,14 +21,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        izah
-        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
-
         data = self.dataset()
 
         if start >= len(data):
