cc2bb771c9b6d203084a667ebc0915ff836004e6
Tahmina Alijewa <tahminaalijewa@gmail.com>
2026-08-08 15:23:43 +0400

    Refactor HTTP server response handling

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,36 +1,33 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const args = process.argv.slice(2);
 const DATABASE = args[0];
-
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   const { url } = req;
 
   if (url === '/') {
-    res.write('Hello Holberton School!');
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    res.write('This is the list of our students\n');
+    res.statusCode = 200;
+    let responseText = 'This is the list of our students\n';
     try {
       const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      res.end(responseText + students.join('\n'));
     } catch (error) {
-      res.end(error.message);
+      res.end(responseText + error.message);
     }
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname, () => {});
 
 module.exports = app;
