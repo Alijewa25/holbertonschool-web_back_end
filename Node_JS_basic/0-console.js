0f3e05dd09d9ce1f5da264090e1a1170e22b62dd
Alijewa25 <tahminaalijewa@gmail.com>
2026-08-08 12:35:58 +0400

    js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
-function displayMessage(input) {
-  console.log(input);
-}
-
+// Use displayMessage function to print a message to the console.
+const displayMessage = (message) => {
+  console.log(message);
+};
 module.exports = displayMessage;
